8af94e2c88f04678f33c871f39e45b6359e5758a
chris <chris@none.non>
2024-10-29 11:12:12 -0400

    Bugfix: Default code tried to cat an object to a string
    That doesn't work in Python!
    
    This was a latent bug in the provided code since the default process_verb should always be overwritten. Students could trigger it by typoing etc.

--- game/context.py
+++ game/context.py
@@ -6,5 +6,4 @@ class Context:
         self.nouns = {}   # in game name of an object
 
     def process_verb (self, verb, cmd_list, nouns):
-        print (self.nouns + " can't " + verb)
-
+        print (str(self.nouns) + " can't " + verb)

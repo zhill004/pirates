230d021e1d16cd8d6459ef43853f752926c66d61
chris <chris@none.non>
2024-10-29 11:12:43 -0400

    Revisions to seagul event.
    Giving them food actually costs the ship food.
    -1 point per 10 seaguls, randomized using integer math.

--- game/events/seagull.py
+++ game/events/seagull.py
@@ -2,8 +2,10 @@ from game import event
 from game.player import Player
 from game.context import Context
 import game.config as config
+import game.display as display
 import random
 
+#Example non-combat event
 class Seagull (Context, event.Event):
     '''Encounter with an annoying seagull. Uses the parser to decide what to do about it.'''
     def __init__ (self):
@@ -31,13 +33,22 @@ class Seagull (Context, event.Event):
                 else:
                     self.result["message"] = c.get_name() + " is attacked by the seagulls."
                     if (c.inflict_damage (self.seagulls, "Pecked to death by seagulls")):
-                        self.result["message"] = ".. " + c.get_name() + " is pecked to death by the seagulls!"
+                        self.result["message"] = f".. {c.get_name()} is pecked to death by the seagulls!"
 
         elif (verb == "feed"):
-            self.seagulls = self.seagulls + 1
-            self.result["newevents"].append (Seagull())
-            self.result["message"] = "the seagulls are happy"
-            self.go = True
+            if (config.the_player.ship.get_food() > 0):
+                if(self.seagulls//10 > 0): #if there's more than 10 seagulls, this definitely costs at least 1 food
+                    config.the_player.ship.take_food ((self.seagulls//10))
+                if(self.seagulls%10 > 0): #if there's a remainder, there's a chance of losing a(n additional) food
+                    if(random.randint(1,10) < (self.seagulls%10)):
+                        config.the_player.ship.take_food (1)
+                self.seagulls = self.seagulls + 1
+                self.result["newevents"].append (Seagull())
+                self.result["message"] = "the seagulls are happy"
+                self.go = True
+            else:
+                display.announce("You have no food to feed the seagulls. This does not bode well for you.")
+                self.go = False
         elif (verb == "help"):
             print ("the seagulls will pester you until you feed them or chase them off")
             self.go = False
